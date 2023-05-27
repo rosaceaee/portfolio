88eae6193d01c11f00bef0997c63bbc5810508a8
yj <corydalisss@gmail.com>
2023-05-27 17:15:39 +0900

    reverted of works component

--- projj/src/pages/Works.js
+++ projj/src/pages/Works.js
@@ -170,8 +170,6 @@ const Works = (langlang) => {
   // console.log(changeLang);
   //const final = worklists.map(({id, name}: {id: string, name: string}) => ({id, name}));
 
-  {
-    /*
   function LangBtn({ setLang }) {
     return (
       <>
@@ -185,9 +183,6 @@ const Works = (langlang) => {
     );
   }
 
-*/
-  }
-
   function Parent({ setLang, haha }) {
     const mapper = new Map([
       ["1", "a"],
@@ -198,8 +193,8 @@ const Works = (langlang) => {
     return (
       <>
         {" "}
-        {/*<LangBtn setLang={setLang} /> 
-        <Header />*/}
+        <LangBtn setLang={setLang} />
+        <Header />
         <nav>
           <p>web</p>
 
@@ -285,6 +280,25 @@ const Works = (langlang) => {
                   </>
                 );
               })}
+            {/* 
+            {worklists.web.map((a, id, b) => {
+              return (
+                <>
+                  <div>
+                    <ToggleItem
+                      id={id}
+                      descc={a.desc[0]}
+                      link={a.desc[1]}
+                      link={row[2]}
+                      subDesc={a.desc[2]}
+                      etcDesc={a.desc[3]}
+                    />
+                    ;
+                  </div>
+                </>
+              );
+            })}
+            */}
           </section>
           <section className="works-con app-con" ref={aboutSection}>
             <h2 className="tit">App</h2>
@@ -307,6 +321,103 @@ const Works = (langlang) => {
     <>
       <main>
         <Parent setLang={setLang} />
+        {/*
+        <section style={{ border: "1px solid", height: "100vh" }}>
+          <section className="works-con">
+            <h2 className="tit">Web</h2>
+            {worklists.web.map((a, id, b) => {
+              return (
+                <>
+                  <div>
+                    <ToggleItem
+                      id={id}
+                      descc={a.desc[0]}
+                      link={a.desc[1]}
+                      subDesc={a.desc[2]}
+                      etcDesc={a.desc[3]}
+                    />
+                    ;
+                  </div>
+                </>
+              );
+            })}
+          </section>
+          <section className="works-con app-con" ref={aboutSection}>
+            <h2 className="tit">App</h2>
+            {worklists.app.map((a, id) => {
+              return (
+                <>
+                  <div className="works work-img">
+                    <ToggleItem id={id} descc={a.desc} />;
+                  </div>
+                </>
+              );
+            })}
+          </section>
+        </section>
+*/}
+
+        {/* 
+          <div className="works work-img" onClick={() => test()}>
+            <h4>1</h4>
+            <div className={show ? "desc-on" : "desc"}>
+              <p>웹앱</p>
+              <p>파닉스몬스터</p>
+              <p>유아이 개선 참여</p>
+              <p>https://epublic-phonics.cloubot.com/</p>
+            </div>
+          </div>{" "}
+          <div>
+            <h4>피엠</h4>
+            <p>사내 보안으로 url을 제공할 수 없어 캡처본으로 대체합니다.</p>
+          </div>
+          <div>
+            <h4>ㅁㅁ로펌</h4>
+            <p>사내 보안으로 url을 제공할 수 없어 캡처본으로 대체합니다.</p>
+            <p> 와꾸짜기 100% </p>
+          </div>
+          <div>
+            <h4>ㅁㅁ로펌</h4>
+            <p>사내 보안으로 url을 제공할 수 없어 캡처본으로 대체합니다.</p>
+            <p> 와꾸짜기 100% </p>
+          </div>
+          <div>
+            <h4>ㅁㅁ로펌</h4>
+            <p>사내 보안으로 url을 제공할 수 없어 캡처본으로 대체합니다.</p>
+            <p> 와꾸짜기 100% </p>
+          </div>
+          <div>
+            <h4>ㅁㅁ로펌</h4>
+            <p>사내 보안으로 url을 제공할 수 없어 캡처본으로 대체합니다.</p>
+            <p> 와꾸짜기 100% </p>
+          </div>
+          <div className="works work-img" onClick={() => test()}>
+            <h4>ㅁㅁ로펌</h4>
+            <div className={show ? "desc-on" : "desc"}>
+              <p>사내 보안으로 url을 제공할 수 없어 캡처본으로 대체합니다.</p>
+              <p> 디자인, 퍼블리싱 100% (이미지 선정 제외) </p>
+            </div>
+          </div>
+          {/*<h1 ref={targetRef}>asdf</h1>
+        </section>
+        <section className="works-con app-con" ref={aboutSection}>
+          <h2>App</h2>
+          <div>
+            <h4>교육용1</h4>
+            <p>사내 보안으로 url을 제공할 수 없어 캡처본으로 대체합니다.</p>
+            <p> 와꾸짜기 100% </p>
+          </div>
+          <div>
+            <h4>교육용2</h4>
+            <p>사내 보안으로 url을 제공할 수 없어 캡처본으로 대체합니다.</p>
+            <p> 와꾸짜기 100% </p>
+          </div>
+          <div>
+            <h4>교육용3</h4>
+            <p>사내 보안으로 url을 제공할 수 없어 캡처본으로 대체합니다.</p>
+            <p> 와꾸짜기 100% </p>
+          </div>{" "}
+        </section>*/}
       </main>
     </>
   );
